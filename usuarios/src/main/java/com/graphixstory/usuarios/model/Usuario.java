e2fe36110bfc15d83889dc73fd50da2cdb23b82d
Vina del Mar <vina@duoc.acad>
2025-05-15 20:57:22 -0400

    recuerda poner - m antes de hacer el commit

--- usuarios/src/main/java/com/graphixstory/usuarios/model/Usuario.java
+++ usuarios/src/main/java/com/graphixstory/usuarios/model/Usuario.java
@@ -1,5 +1,41 @@
 package com.graphixstory.usuarios.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+
+@Entity
+@Table (name= "user")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Usuario {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(unique= true, length = 13, nullable=false)
+    private String run;
+
+    @Column(nullable = false)
+    private String nombre;
+
+    @Column(nullable = false)
+    private String apellido;
+
+    @Column(nullable = false)
+    private Date FechaNacimiento;
+
+    
 }
+
